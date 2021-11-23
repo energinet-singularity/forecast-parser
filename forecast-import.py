fbe6438c436a70af1a99e6bca86ebe8acf71aaf5
Karsten Rasmussen <bolsmand@gmail.com>
2021-11-23 16:01:46 +0100

    Made kSQL validation via REST api

--- forecast-import.py
+++ forecast-import.py
@@ -1,8 +1,3 @@
-#TO DO:
-# - Add error handling - there is NONE!
-# - Manage gridpoints in a more clever way
-# - Perhaps improve file-handling
-
 from kafka import KafkaProducer
 import pandas as pd
 import datetime as dt
@@ -10,13 +5,13 @@ import json
 import re                           #Used to identify file-types
 import os                           #Used for file-management and environt variables
 import time                         #Used for sleep-function only
+import requests                     #Used to manage ksql setup
 
-#Variables exposed as system variables
-kafka_topic = os.environ['KAFKA_TOPIC']
-if kafka_topic == "": kafka_topic = "weather-forecast-raw"
-kafka_host=os.environ['KAFKA_HOST']
-
-#Global variables
+#Variables initialized (some are exposed as system variables)
+kafka_topic = os.environ.get('KAFKA_TOPIC', "weather-forecast-raw")
+kafka_host = os.environ.get('KAFKA_HOST', "my-cluster-kafka-brokers")
+ksql_host = os.environ.get('KSQL_HOST', "kafka-cp-ksql-server")
+ksql_setup_valid = False
 forecast_folder = "forecast-files/"
 coords_csv_file = "gridpoints.csv"
 forecast_file_filter = r"(E[Nn]et(NEA|Ecm)_|ConWx_prog_)\d+(_\d{3})?\.(txt|dat)"
@@ -96,6 +91,67 @@ def publish_forecast(producer: KafkaProducer, df: dict, kafka_topic: str, locati
         #Send data to kafka
         producer.send(kafka_topic, json.dumps(json_item))
 
+def setup_ksql(kafka_topic: str, ksql_host: str):
+    #Setup of ksql streams and tables
+    ksql_setup=[
+        {'TYPE': 'STREAM', 'NAME': 'STREAM_FORECAST_RAW', 'CONFIG': f"CREATE stream STREAM_FORECAST_RAW (forecast_type VARCHAR,estimation_time VARCHAR,estimation_source VARCHAR,position_lon DOUBLE,position_lat DOUBLE,forecast_time ARRAY<VARCHAR>,temperature_2m ARRAY<DOUBLE>,temperature_100m ARRAY<DOUBLE>,wind_speed_10m ARRAY<DOUBLE>,wind_direction_10m ARRAY<DOUBLE>,wind_speed_100m ARRAY<DOUBLE>,wind_direction_100m ARRAY<DOUBLE>,direct_radiation ARRAY<DOUBLE>,global_radiation ARRAY<DOUBLE>,accumulated_global_radiation ARRAY<DOUBLE>) WITH (kafka_topic={kafka_topic},value_format = 'json');"},
+        {'TYPE': 'STREAM', 'NAME': 'STREAM_FORECAST_CONWX', 'CONFIG': "CREATE STREAM STREAM_FORECAST_CONWX AS SELECT estimation_time,position_lon,position_lat,EXPLODE(temperature_2m) AS temperature_2m,EXPLODE(temperature_100m) AS temperature_100m,EXPLODE(wind_speed_10m) AS wind_speed_10m,EXPLODE(wind_direction_10m) AS wind_direction_10m,EXPLODE(wind_speed_100m) AS wind_speed_100m,EXPLODE(wind_direction_100m) AS wind_direction_100m,EXPLODE(direct_radiation) AS direct_radiation,EXPLODE(global_radiation) AS global_radiation,EXPLODE(accumulated_global_radiation) AS accumulated_global_radiation,EXPLODE(forecast_time) AS forecast_time FROM STREAM_FORECAST_RAW WHERE forecast_type = 'ConWx';"},
+        {'TYPE': 'TABLE', 'NAME': 'FORECAST_CONWX', 'CONFIG': "CREATE TABLE FORECAST_CONWX WITH (KEY_FORMAT='JSON') AS SELECT forecast_time,position_lon,position_lat,LATEST_BY_OFFSET(temperature_2m) AS temperature_2m,LATEST_BY_OFFSET(temperature_100m) AS temperature_100m,LATEST_BY_OFFSET(wind_speed_10m) AS wind_speed_10m,LATEST_BY_OFFSET(wind_speed_100m) AS wind_speed_100m,LATEST_BY_OFFSET(wind_direction_10m) AS wind_direction_10m,LATEST_BY_OFFSET(wind_direction_100m) AS wind_direction_100m,LATEST_BY_OFFSET(direct_radiation) AS direct_radiation,LATEST_BY_OFFSET(global_radiation) AS global_radiation,LATEST_BY_OFFSET(accumulated_global_radiation) AS accumulated_global_radiation FROM STREAM_FORECAST_CONWX GROUP BY forecast_time,position_lon,position_lat EMIT CHANGES;"},
+        {'TYPE': 'STREAM', 'NAME': 'STREAM_FORECAST_DMI', 'CONFIG': "CREATE STREAM STREAM_FORECAST_DMI AS SELECT estimation_time,position_lon,position_lat,EXPLODE(temperature_2m) AS temperature_2m,EXPLODE(temperature_100m) AS temperature_100m,EXPLODE(wind_speed_10m) AS wind_speed_10m,EXPLODE(wind_direction_10m) AS wind_direction_10m,EXPLODE(wind_speed_100m) AS wind_speed_100m,EXPLODE(wind_direction_100m) AS wind_direction_100m,EXPLODE(direct_radiation) AS direct_radiation,EXPLODE(global_radiation) AS global_radiation,EXPLODE(accumulated_global_radiation) AS accumulated_global_radiation,EXPLODE(forecast_time) AS forecast_time FROM STREAM_FORECAST_RAW WHERE forecast_type != 'ConWx';"},
+        {'TYPE': 'TABLE', 'NAME': 'FORECAST_DMI', 'CONFIG': "CREATE TABLE FORECAST_DMI WITH (KEY_FORMAT='JSON') AS SELECT forecast_time,position_lon,position_lat,LATEST_BY_OFFSET(temperature_2m) AS temperature_2m,LATEST_BY_OFFSET(temperature_100m) AS temperature_100m,LATEST_BY_OFFSET(wind_speed_10m) AS wind_speed_10m,LATEST_BY_OFFSET(wind_speed_100m) AS wind_speed_100m,LATEST_BY_OFFSET(wind_direction_10m) AS wind_direction_10m,LATEST_BY_OFFSET(wind_direction_100m) AS wind_direction_100m,LATEST_BY_OFFSET(direct_radiation) AS direct_radiation,LATEST_BY_OFFSET(global_radiation) AS global_radiation,LATEST_BY_OFFSET(accumulated_global_radiation) AS accumulated_global_radiation FROM STREAM_FORECAST_DMI GROUP BY forecast_time,position_lon,position_lat EMIT CHANGES;"}
+    ]
+
+    #Verifying connection
+    print(f"Validating kSQLdb setup on host '{ksql_host}'..")
+    
+    if requests.get(f"http://{ksql_host}:8088/info").status_code == 200:
+        print('Host responded in an orderly fashion..')
+    else:
+        print(f"Rest API on 'http://{ksql_host}:8088/info' did not respond as expected.")
+        print('Resuming normal operation - will check again later..')
+        return False
+
+    #Verifying streams are set up correctly
+    response = requests.post(f"http://{ksql_host}:8088/ksql",json={"ksql": "LIST STREAMS;", "streamsProperties": {}})
+    if response.status_code == 200:
+        stream_dict = [stream['name'] for stream in response.json().pop()['streams']]
+        for ksql_config in ksql_setup:
+            if ksql_config['TYPE'] == 'STREAM':
+                if ksql_config['NAME'] in stream_dict:
+                    print(f'Stream \'{ksql_config["NAME"]}\' found.')
+                else:
+                    response = requests.post(f"http://{ksql_host}:8088/ksql",json={"ksql": ksql_config['CONFIG'], "streamsProperties": {}})
+                    if response.json().pop()['commandStatus']['status'] == 'SUCCESS':
+                        print(f'Stream \'{ksql_config["NAME"]}\' created.')
+                    else:
+                        print(f'Problem while trying to create stream \'{ksql_config["NAME"]}\' - will retry again later..')
+                        return False
+    else:
+        print('Could not list streams - will check again later..')    
+        return False
+    
+    #Verifying tables are set up correctly
+    response = requests.post(f"http://{ksql_host}:8088/ksql",json={"ksql": "LIST TABLES;", "streamsProperties": {}})
+    if response.status_code == 200:
+        table_dict = [table['name'] for table in response.json().pop()['tables']]
+        for ksql_config in ksql_setup:
+            if ksql_config['TYPE'] == 'TABLE':
+                if ksql_config['NAME'] in table_dict:
+                    print(f'Table \'{ksql_config["NAME"]}\' found.')
+                else:
+                    response = requests.post(f"http://{ksql_host}:8088/ksql",json={"ksql": ksql_config['CONFIG'], "streamsProperties": {}})
+                    if response.json().pop()['commandStatus']['status'] == 'SUCCESS':
+                        print(f'Table \'{ksql_config["NAME"]}\' created.')
+                    else:
+                        print(f'Problem while trying to create table \'{ksql_config["NAME"]}\' - will retry again later..')
+                        return False
+    else:
+        print('Could not list tables - will check again later..')    
+        return False
+    
+    print('kSQL setup has been validated.')
+    return True
+
 if __name__ == "__main__":
     print("Starting 'main' routine..")
 
@@ -131,6 +187,11 @@ if __name__ == "__main__":
 
     #Iterate through folder for files
     while True:
+        #Check kSQLdb has been set up, otherwise reconfigure it.
+        if not ksql_setup_valid:
+            if setup_ksql(kafka_topic, ksql_host): ksql_setup_valid = True
+        
+        #Do the main loop / check for files
         print("Checking folder for new files..")
         for root, directories, files in os.walk(forecast_folder):
             for filename in files:
